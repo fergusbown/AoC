33def081c2d3448e15761794b463f8dc425cd083
Fergus Bown <fergus.bown@towerswatson.com>
2021-12-13 11:49:42 +0000

    More tidying

--- AoC2021/AoC2021Runner/Span2DExtensions.cs
+++ AoC2021/AoC2021Runner/Span2DExtensions.cs
@@ -4,19 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Toolkit.HighPerformance;
+using Microsoft.Toolkit.HighPerformance.Enumerables;
 
 namespace AoC2021Runner
 {
     public static class Span2DExtensions
     {
-        public static void FlipHorizontally<T>(this Span2D<T> span)
+        public static void ReverseColumns<T>(this Span2D<T> span)
         {
-            int swappingColumn = span.Width - 1;
+            int firstColumnIndex = span.Width - 1;
             Span<T> temp = new Span<T>(new T[span.Height]);
-            for (int foldingColumn = 0; foldingColumn < span.Width / 2; foldingColumn++, swappingColumn--)
+            for (int secondColumnIndex = 0; secondColumnIndex < span.Width / 2; secondColumnIndex++, firstColumnIndex--)
             {
-                var swapping1 = span.GetColumn(swappingColumn);
-                var swapping2 = span.GetColumn(foldingColumn);
+                RefEnumerable<T> swapping1 = span.GetColumn(firstColumnIndex);
+                RefEnumerable<T> swapping2 = span.GetColumn(secondColumnIndex);
 
                 swapping1.CopyTo(temp);
                 swapping2.CopyTo(swapping1);
@@ -24,14 +25,14 @@ namespace AoC2021Runner
             }
         }
 
-        public static void FlipVertically<T>(this Span2D<T> span)
+        public static void ReverseRows<T>(this Span2D<T> span)
         {
-            int swappingRow = span.Height - 1;
+            int firstRowIndex = span.Height - 1;
             Span<T> temp = new Span<T>(new T[span.Width]);
-            for (int foldingRow = 0; foldingRow < span.Height / 2; foldingRow++, swappingRow--)
+            for (int secondRowIndex = 0; secondRowIndex < span.Height / 2; secondRowIndex++, firstRowIndex--)
             {
-                var swapping1 = span.GetRowSpan(swappingRow);
-                var swapping2 = span.GetRowSpan(foldingRow);
+                Span<T> swapping1 = span.GetRowSpan(firstRowIndex);
+                Span<T> swapping2 = span.GetRowSpan(secondRowIndex);
                 swapping1.CopyTo(temp);
                 swapping2.CopyTo(swapping1);
                 temp.CopyTo(swapping2);
