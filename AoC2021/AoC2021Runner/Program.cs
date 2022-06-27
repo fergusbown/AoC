1fcdd3232148507a95b97b0ca7c5819f9fadf4b4
Fergus Bown <fergus.bown@towerswatson.com>
2022-06-27 15:04:28 +0100

    Finally add some startup parameters

--- AoC2021/AoC2021Runner/Program.cs
+++ AoC2021/AoC2021Runner/Program.cs
@@ -1,19 +1,23 @@
 ﻿using System.Diagnostics;
 using System.Reflection;
 using AoC2021Runner;
+using CommandLine;
 
-var daySolutions = TimeOperation(() => GetSolutions(args), (s, t) => $"Initialised{t}");
-
-foreach ((int year, int day, IDayChallenge solution) in daySolutions)
+var options = Parser.Default.ParseArguments<Options>(args).WithParsed(o =>
 {
-    Console.WriteLine($"{year} day {day}...");
-    _ = TimeOperation(solution.Part1, (s, t) => $"  Part 1{t}: {s}");
-    _ = TimeOperation(solution.Part2, (s, t) => $"  Part 2{t}: {s}");
-}
+    var daySolutions = TimeOperation(() => GetSolutionImplementations(GetSolutionDefinitions(o)), (s, t) => $"Initialised{t}");
 
-static IEnumerable<(int year, int Day, IDayChallenge Solution)> GetSolutions(string[] args)
+    foreach ((int year, int day, IDayChallenge solution) in daySolutions)
+    {
+        Console.WriteLine($"{year} day {day}...");
+        _ = TimeOperation(solution.Part1, (s, t) => $"  Part 1{t}: {s}");
+        _ = TimeOperation(solution.Part2, (s, t) => $"  Part 2{t}: {s}");
+    }
+});
+
+static IEnumerable<(int Year, int Day, Type Type)> GetSolutionDefinitions(Options options)
 {
-    return Assembly
+    var result = Assembly
         .GetExecutingAssembly()
         .GetTypes()
         .Where(t => t.IsAssignableTo(typeof(IDayChallenge)) && t.IsClass)
@@ -22,20 +26,25 @@ static IEnumerable<(int year, int Day, IDayChallenge Solution)> GetSolutions(str
             (int year, int day, Type type) result = (int.Parse(t.Name[4..8]), int.Parse(t.Name[9..]), t);
             return result;
         })
-        .Where(t => t.year == 2020)
+        .Where(t => options.Year is null || t.year == options.Year)
         .OrderBy(t => t.year)
-        .ThenBy(t => t.day)
-        .TakeLast(1)
-        .Select(t =>
-        {
-            string? inputData = InputData.InputForDay(t.type);
+        .ThenBy(t => t.day);
 
-            var instance = inputData is null ? Activator.CreateInstance(t.type) : Activator.CreateInstance(t.type, inputData);
+    return options.LatestOnly ? result.TakeLast(1) : result;
+}
 
-            (int year, int day, IDayChallenge solution) result = (t.year, t.day, (IDayChallenge)instance!);
-            return result;
-        })
-        .ToList();
+static IEnumerable<(int Year, int Day, IDayChallenge Solution)> GetSolutionImplementations(IEnumerable<(int Year, int Day, Type Type)> definitions)
+{
+    return definitions.Select(t =>
+    {
+        string? inputData = InputData.InputForDay(t.Type);
+
+        var instance = inputData is null ? Activator.CreateInstance(t.Type) : Activator.CreateInstance(t.Type, inputData);
+
+        (int, int, IDayChallenge) result = (t.Year, t.Day, (IDayChallenge)instance!);
+        return result;
+    })
+    .ToList();
 }
 
 static T TimeOperation<T>(Func<T> operation, Func<T, string, string> logFunction)
@@ -46,4 +55,13 @@ static T TimeOperation<T>(Func<T> operation, Func<T, string, string> logFunction
     Console.WriteLine(logFunction(result, $" ({sw.ElapsedMilliseconds}ms)"));
 
     return result;
+}
+
+public class Options
+{
+    [Option('v', "year", Required = false)]
+    public int? Year { get; set; }
+
+    [Option('l', "latest", Required = false)]
+    public bool LatestOnly { get; set; }
 }
