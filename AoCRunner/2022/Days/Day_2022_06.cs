abd33207324299fca9b02c2213807ded0252fb01
Fergus Bown <fergus.bown@towerswatson.com>
2022-12-07 06:50:21 +0000

    2022 day 6 and 7

---
+++ AoCRunner/2022/Days/Day_2022_06.cs
@@ -0,0 +1,34 @@
+﻿namespace AoCRunner;
+
+internal class Day_2022_06 : IDayChallenge
+{
+    private readonly string inputData;
+
+    public Day_2022_06(string inputData)
+    {
+        this.inputData = inputData;
+    }
+
+    public string Part1()
+    {
+        return FindStart(this.inputData, 4).ToString();
+    }
+
+    public string Part2()
+    {
+        return FindStart(this.inputData, 14).ToString();
+    }
+
+    static int FindStart(string input,int length)
+    {
+        for (int i = 0; i < input.Length; i++)
+        {
+            if (input.Skip(i).Take(length).Distinct().Count() == length)
+            {
+                return i + length;
+            }
+        }
+
+        return -1;
+    }
+}
---
+++ AoCRunner/2022/Days/Day_2022_07.cs
@@ -0,0 +1,104 @@
+﻿namespace AoCRunner;
+
+internal class Day_2022_07 : IDayChallenge
+{
+    private readonly Directory inputData;
+
+    public Day_2022_07(string inputData)
+    {
+        this.inputData = Directory.Read(inputData);
+    }
+
+    public string Part1()
+    {
+        return inputData
+            .AllSubdirectories
+            .Select(d => d.TotalSize)
+            .Where(d => d <= 100000)
+            .Sum()
+            .ToString();
+    }
+
+    public string Part2()
+    {
+        int spaceFree = 70000000 - inputData.TotalSize;
+        int spaceToFree = 30000000 - spaceFree;
+
+        return inputData
+            .AllSubdirectories
+            .Select(d => d.TotalSize)
+            .Where(d => d >= spaceToFree)
+            .OrderBy(d => d)
+            .First()
+            .ToString();
+    }
+
+    private class Directory
+    {
+        private readonly Dictionary<string, Directory> subdirectories = new();
+        private int size = 0;
+
+        private Directory()
+        {
+
+        }
+
+        public string Name { get; init; } = string.Empty;
+
+        public Directory? Parent { get; init; } = null;
+
+        public Directory this[string subdrectory] => subdrectory switch
+        {
+            ".." => Parent!,
+            _ => subdirectories[subdrectory],
+        };
+
+        public int TotalSize => size + subdirectories.Values.Select(d => d.TotalSize).Sum();
+
+        public IEnumerable<Directory> AllSubdirectories
+            => this.subdirectories.Values.Concat(this.subdirectories.Values.SelectMany(d => d.AllSubdirectories));
+
+        public static Directory Read(string inputData)
+        {
+            string[] output = inputData.StringsForDay();
+
+            Directory result = new Directory();
+            Directory current = result;
+
+            foreach (string s in output.Skip(1))
+            {
+                string[] parts = s.Split(' ');
+                if (parts[0] == "$")
+                {
+                    if (parts[1] == "cd")
+                    {
+                        current = current[parts[2]];
+                    }
+                }
+                else
+                {
+                    if (parts[0] == "dir")
+                    {
+                        current.AddDirectory(parts[1]);
+                    }
+                    else
+                    {
+                        current.AddFile(int.Parse(parts[0]));
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        private void AddDirectory(string name)
+        {
+            subdirectories.Add(name, new Directory { Name = name, Parent = this });
+        }
+
+        private void AddFile(int size)
+        {
+            this.size += size;
+        }
+    }
+}
